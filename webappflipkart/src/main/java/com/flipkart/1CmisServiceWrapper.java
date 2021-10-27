ec8a6a8b062a0fbd5d5627ed8910c66118303d58
Paramgithub-stack <paramesh.2bnglr@gmail.com>
2021-10-27 03:44:54 -0700

    1commit

---
+++ webappflipkart/src/main/java/com/flipkart/1CmisServiceWrapper.java
@@ -0,0 +1,19 @@
+package com.flipkart;
+
+import org.apache.chemistry.opencmis.commons.server.CmisService;
+import org.apache.chemistry.opencmis.server.support.wrapper.AbstractCmisServiceWrapper;
+
+/**
+ * CMIS Service Wrapper.
+ */
+public class 1CmisServiceWrapper extends AbstractCmisServiceWrapper {
+
+    public 1CmisServiceWrapper(CmisService service) {
+        super(service);
+    }
+    
+    // @Override
+    // public void initialize(Object[] params) {
+    // }
+
+}
